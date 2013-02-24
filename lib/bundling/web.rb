49bd1ec11f77caec6d57b1bb5faa33f3816f890f
Carl Woodward <carl@88cartell.com>
2013-02-24 23:08:45 +1100

    Adds initial web interface.

--- lib/bundling/web.rb
+++ lib/bundling/web.rb
@@ -2,5 +2,12 @@ require "sinatra/base"
 
 module Bundling
   class Web < Sinatra::Base
+    get "/" do
+      if params.any?
+        status 200
+      else
+        not_found
+      end
+    end
   end
 end
--- spec/bundling/web_spec.rb
+++ spec/bundling/web_spec.rb
@@ -7,4 +7,20 @@ describe Bundling::Web do
   def app
     Bundling::Web.new
   end
+
+  describe "GET /" do
+    context "without any gems" do
+      it "returns 404" do
+        get "/"
+        expect(last_response.status).to be 404
+      end
+    end
+
+    context "with gems and versions" do
+      it "returns dependencies" do
+        get "/?sinatra=latest&rake=0.9.5"
+        expect(last_response.status).to be 200
+      end
+    end
+  end
 end
